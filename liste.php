915258d0515d9106ad65695040dc358709bd6a40
zakaria-lahrach <64549943+zakaria-lahrach@users.noreply.github.com>
2022-09-26 15:17:01 +0100

    Update liste.php

--- liste.php
+++ liste.php
@@ -4,5 +4,9 @@ require_once("db.php");
 $pdo_statement = $pdo_conn->prepare("SELECT * FROM todo");
 $pdo_statement->execute();
 $result = $pdo_statement->fetchAll();
-print_r($result);
+$data = [];
+foreach ( $result as $row ) {
+    $data[]= ["id"=>$row['id'],"title"=>$row['title'],"description"=>$row['description']];
+}
+echo json_encode($data);
 ?>
